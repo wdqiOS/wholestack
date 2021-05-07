3d2c477951c0e6c0e78a291fd9c8c9316583c55e
haiping.liu <code.server@joyfulengine.com>
2021-05-07 21:45:12 +0800

    wdq2021年5月7日21:45:10

---
+++ JavaScript基础/01-JavaScript书写格式.js
@@ -0,0 +1,10 @@
+// alert('您好，李哲')
+
+// var oDiv = document.querySelector('div');
+// var text= oDiv.innerText;
+// alert(text);
+window.onload = function() {
+	var oDiv = document.querySelector('div');
+	var text= oDiv.innerText;
+	alert(text);
+}
